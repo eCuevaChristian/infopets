50cc0ca0428f358767cef7c8e6a3cf3b4dd357db
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-07-03 16:10:37 -0500

    modifico legunaje en react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import './assets/css/componentes/card.css';
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
 
 function App() {
   return (
     <>
-
+    <Home/>
+    <Sobre/>
     </>
   );
 }
