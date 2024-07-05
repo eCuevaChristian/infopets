0885a90aca3a6e3e04007745dc1f2a51f385b98d
Christian Eduardo Taipe Cueva <christianecueva@gmail.com>
2024-07-04 23:07:11 -0500

    Crea ListPost

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page404 from './pages/Page404';
+import Header from './components/Header';
 
 function App() {
 
 
   return (
     <Router>
+      <Header></Header>
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/sobre' element={ <Sobre /> } />
